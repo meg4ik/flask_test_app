e8cd807ea4c47ef2c1ec011f000f56029d5c3b02
meg4ik <meg4mag@gmail.com>
2020-11-27 16:52:59 +0200

    Revert "add ORM"
    This reverts commit ee3dedbf93a37249427c55d4d5f2c8b5bf7f3c8e.

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 from flask import Flask, request
 from simple_settings import settings
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
+import pymysql as MySQLdb
 
 app = Flask(__name__)
-
 app.config.update(**settings.as_dict())
-
-
-db = SQLAlchemy(app)
-
-class person(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(45), nullable=False)
-    age = db.Column(db.Integer, nullable = False)
-    job = db.Column(db.String(45))
-
 
 @app.route("/", methods=["GET","POST"])
 def home():
     return "hello", 200
 
 if __name__ == "__main__":
-    db.create_all()
-
-    person.query.delete()
-
-    Van = person(name= "Van", age = 3, job = "ATB")
-    
-    db.session.add(Van)
-    db.session.commit()
-
     app.run()
